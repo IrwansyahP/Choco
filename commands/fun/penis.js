88c5820a77e0f30b395bf90502d3b12c730a18fa
IrwansyahP <71760612+IrwansyahP@users.noreply.github.com>
2021-05-03 15:28:15 +0700

    Create penis.js

---
+++ commands/fun/penis.js
@@ -0,0 +1,12 @@
+module.exports = ({
+    name: "penis",
+    aliases: ['dick'],
+    code:`
+$title[**Penis Size Machine**]
+$description[<@$mentioned[1]> Penis Size:
+**8$randomText[=;==;===;====;=====;======;=======;========;=========;==========;===========;============;=============;==============;===============;================;=================;==================;==================;===================;====================]D**]
+$footer[]
+$color[#CAEAF9]
+$addTimestamp
+$argsCheck[>1;{title: Error}{description: **Ussage:** \`c.penis <mention>\`}{color: #ff0000}]`
+})
