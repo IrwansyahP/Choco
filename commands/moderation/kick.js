59680de8883f86defd36ed7deeadb843c97b639a
IrwansyahP <71760612+IrwansyahP@users.noreply.github.com>
2021-05-03 15:19:50 +0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,43 +1,25 @@
 module.exports = ({
  name: "kick",
- code: `
- $kick[$mentioned[1]]
-$if[$mentioned[1]==]
-Anda Harus Tag Seseorang!
-$elseIf[$message[2]==]
-$author[$username[$mentioned[1]] Telah Di Kick!;$userAvatar[$mentioned[1]]]
-$description[
+ usage: "kick <user>",
+ description: "Kick A Member",
+ perms: "kick",
+ code: `$kick[$findMember[$message;no]]
+ $title[Kick]
+ $description[
+ **Successfully Kicked:**
  **Tag:** $userTag[$findUser[$message[1]]]
  **Mention:** <@$findUser[$message[1]]>
- **ID:** $findUser[$message[1]]]
- $addField[Di Kick Oleh:; $userTag[$authorID]]
-$footer[]
-$addTimestamp
-$addField[Alasan:; \`Tidak Ada Alasan\`]
-$color[RED]
-$sendDM[$mentioned[1];Kamu Telah Di Kick Dari Server **$serverName**, Karena: \`Tidak Ada Alasan\`]
-$endelseIf
-$elseIf[$message[2]==$message[2]]
-$author[$username[$mentioned[1]] Telah Di Kick!;$userAvatar[$mentioned[1]]]
-$description[
- **Tag:** $userTag[$findUser[$message[1]]]
- **Mention:** <@$findUser[$message[1]]>
- **ID:** $findUser[$message[1]]]
- $addField[**Di Kick Oleh:**; $userTag[$authorID]]
-$addField[**Alasan:**; \`$noMentionMessage\`]
-$footer[]
-$addTimestamp
-$color[RED]
-$sendDM[$mentioned[1];Kamu Telah Di Kick Dari Server **$serverName**, Karena: \`$noMentionMessage\`]
-$endelseIf
-$endif
+ **ID:** $findUser[$message[1]]
 
-$onlyIf[$findUser[$message[1]]!=$clientID;<@$authorID>, Saya Tidak Bisa Kick Diri Saya Sendiri!]
-$onlyIf[$findUser[$message[1]]!=$authorID;<@$authorID>, Kamu Tidak Bisa Kick Dirimu Sendiri!]
-$onlyIf[$rolePosition[$highestRole[$authorID]]<$rolePosition[$highestRole[$findUser[$message[1]]]];<@$authorID>, Kamu Tidak Bisa Kick Seseorang Yang Rolenya Lebih Tinggi Daripada Rolemu!]
-$onlyIf[$rolePosition[$highestRole[$clientID]]<$rolePosition[$highestRole[$findUser[$message[1]]]];<@$authorID>, Saya Tidak Bisa Kick Seseorang Yang Rolenya Lebih Tinggi Daripada Role Saya!]
-$onlyIf[$memberExists[$findUser[$message[1]]]==true;<@$authorID>, Saya Tidak Bisa Menemukan Dia!]
-$onlyIf[$findUser[$message[1]]!=$ownerID;<@$authorID>, Kamu Tidak Bisa Kick Owner Server!]
-$onlyBotPerms[kick;<@$authorID>, Saya Tidak Ada \`KICK\` Permission!]
-$onlyPerms[kick;<@$authorID>, Kamu Tidak Ada \`KICK\` Permission!]`
+**Reason:**
+$noMentionMessage]
+ $footer[Kicked By: $username[$authorID]]
+ $addTimestamp
+ $color[GREEN]
+ $onlyIf[$findMember[$message;no]!=$authorID;{title: Error} {description: Something Went Error!} {color: #ff0000}]
+ $onlyIf[$userExists[$findMember[$message;no]]==true;{title: :mg_reactno: Error!} {description: That User Doesn't Exist!} {color: #fc0000}]
+ $argsCheck[1;{title: :Error} {description: **Ussage:** \`c.kick <mention> <reason>\`} {color: #ff0000}]
+ $onlyPerms[kick;{title: :Error} {description: You Don't Have \`kick\` Permission!} {color: #ff0000}]
+$onlyBotPerms[kick;{title: Error} {description: I Don't Have \`kick\` Permission!} {color: #ff0000}]
+ `
 });
